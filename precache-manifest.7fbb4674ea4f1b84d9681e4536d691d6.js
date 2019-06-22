076c61b3a15f9e96acd6e66fd9254b97ff6a8dd0
Greg Cruz <therealcruzer@outlook.com>
2019-06-21 18:34:52 -0700

    Updates

---
+++ precache-manifest.7fbb4674ea4f1b84d9681e4536d691d6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17680c13fa9b2e3543a754fd443c4804",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d3954a6e22b44d9b8f7a",
+    "url": "/portfolio/static/css/2.1e871fd3.chunk.css"
+  },
+  {
+    "revision": "bf4c891f5648b344e342",
+    "url": "/portfolio/static/css/main.adf9e3cd.chunk.css"
+  },
+  {
+    "revision": "d3954a6e22b44d9b8f7a",
+    "url": "/portfolio/static/js/2.0893b43f.chunk.js"
+  },
+  {
+    "revision": "bf4c891f5648b344e342",
+    "url": "/portfolio/static/js/main.864a51db.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "561459102460bffca8595a9dc2e98aec",
+    "url": "/portfolio/static/media/Greg.56145910.jpg"
+  },
+  {
+    "revision": "6a4c4eef1c9ed373b0afdabf7addc15b",
+    "url": "/portfolio/static/media/forest-background.6a4c4eef.jpg"
+  },
+  {
+    "revision": "6a7f63dafdcd2b1be1821c6847117d9f",
+    "url": "/portfolio/static/media/plane-background.6a7f63da.jpg"
+  },
+  {
+    "revision": "de9c99687ac2d51a08e4539d2aa92d16",
+    "url": "/portfolio/static/media/sky-background.de9c9968.jpg"
+  },
+  {
+    "revision": "b3778cdd9fbf7e4a1b5afee6a03b1f8f",
+    "url": "/portfolio/static/media/space-background.b3778cdd.jpg"
+  }
+]);
